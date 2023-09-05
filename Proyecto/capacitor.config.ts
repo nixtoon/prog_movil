c50f4009b2a10b211e63d77136f0ff2b3fab04bd
nixtoon <127158636+nixtoon@users.noreply.github.com>
2023-09-05 18:52:01 -0300

    main

--- Proyecto/capacitor.config.ts
+++ Proyecto/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'myApp',
+  appName: 'myAppli',
   webDir: 'www',
   server: {
     androidScheme: 'https'
--- Proyecto/src/app/app.module.ts
+++ Proyecto/src/app/app.module.ts
@@ -6,11 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations' //1ra modificacion, importar BrowserAnimationsModule
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule], // 2da modificacion, agregarlo al modulo
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
